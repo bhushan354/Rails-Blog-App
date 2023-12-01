dc6bd351d4d37b8cb0f3e68263117f1e13940264
bhushan354 <deshmukhumesh354@gmail.com>
2023-12-01 21:09:19 +0530

    Add create posts table

---
+++ db/migrate/20231201192630_create_posts.rb
@@ -0,0 +1,18 @@
+class CreatePosts < ActiveRecord::Migration[7.1]
+    def change
+        create_table :posts do |t|
+            t.integer :author_id
+            t.string :title
+            t.text :text
+            t.integer :comments_counter, default: 0
+            t.integer :posts_counter, default: 0
+            t.integer
+            :likes_counter,
+            default: 0
+
+            t.foreign_key :users, column: :author_id
+            t.index :author_id
+            t.timestamps
+        end
+    end
+end
