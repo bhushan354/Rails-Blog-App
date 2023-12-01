717e11451c0a9a465c9406b9b2d026cdcfdbb673
bhushan354 <deshmukhumesh354@gmail.com>
2023-12-01 19:28:19 +0530

    Add CreateUsers class

---
+++ db/migrate/20231201181830_create_users.rb
@@ -0,0 +1,12 @@
+class CreateUsers < ActiveRecord::Migration[7:1]
+   def change
+        create_table :users do |t| 
+            t.string :name, null: false
+            t.string :photo,
+            t.text :bio
+
+            t.index :photo, unique: true
+            t.timestamps
+        end
+    end
+end
