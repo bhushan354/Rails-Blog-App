86a5f7d6f0de2360f32378d9aad54a7b2b8eff07
bhushan354 <deshmukhumesh354@gmail.com>
2023-12-02 15:01:04 +0530

    Add create likes table and run a migration on createuser

--- db/migrate/20231201181830_create_users.rb
+++ db/migrate/20231201181830_create_users.rb
@@ -1,12 +1,13 @@
-class CreateUsers < ActiveRecord::Migration[7:1]
-   def change
-        create_table :users do |t| 
-            t.string :name, null: false
-            t.string :photo,
-            t.text :bio
-
-            t.index :photo, unique: true
-            t.timestamps
-        end
+class CreateUsers < ActiveRecord::Migration[7.1]
+    def change
+      create_table :users do |t| 
+        t.string :name, null: false
+        t.string :photo
+        t.text :bio
+  
+        t.index :photo, unique: true
+        t.timestamps
+      end
     end
-end
+  end
+  
