454c0bbe3393777664c086a3d23adcc15afccb14
bhushan354 <deshmukhumesh354@gmail.com>
2023-12-02 15:07:42 +0530

    Run migration for create comment and fix typo in create comments

--- db/migrate/20231202132530_create_comments.rb
+++ db/migrate/20231202132530_create_comments.rb
@@ -1,7 +1,7 @@
-class CreateComments < ActiveRecord::Migration[7:1]
+class CreateComments < ActiveRecord::Migration[7.1]
     def change
         create_table :comments do |t|
-            t.integer :post_id, numm: false
+            t.integer :post_id, null: false
             t.integer :user_id, null: false
             t.text :text
 
